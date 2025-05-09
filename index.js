f2a1174224a9bf2a6d19a2bb50e11a9dd585a189
Jaswanth3204 <jaswanthganji@ro-one.in>
2025-05-09 12:41:35 +0530

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3001;
 
 // Enable CORS for frontend at http://localhost:3000
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3002',
     credentials: true
 }));
 
